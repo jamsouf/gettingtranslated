70a23c537edcb94804340f148322e321a71f51f3
jamsouf <jamil@soufan.de>
2017-02-02 13:50:11 +0100

    - More basic stuff - Remove unused default/example files

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Web-based Translation Editor</title>
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('libs/uikit/css/uikit.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/default.css') }}">
     <script src="{{ asset('libs/vue/vue.min.js') }}"></script>
@@ -15,7 +16,7 @@
 <div uk-grid uk-sticky>
     <div class="uk-width-1-1 uk-margin-bottom uk-padding-remove bg-white clr-dark">
         <div class="uk-padding-small">
-            <a href="{{ url('/') }}"><img src="{{ asset('assets/img/logo.png') }}"></a>
+            <a href="{{ url('/') }}"><img src="{{ asset('assets/img/logo.png') }}" width="110"></a>
         </div>
     </div>
 </div>
